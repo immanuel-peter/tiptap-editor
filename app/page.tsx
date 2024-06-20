59e27bd6f84949ceb57456d0732d949009494f23
immanuel.peter1 <immanuel.peter1@gmail.com>
2024-06-20 04:46:59 +0000

    First commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Tiptap from "./tiptap";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <Tiptap />
+    </main>
+  );
+}
